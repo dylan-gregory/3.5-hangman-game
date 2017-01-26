ab0ed2b4db7c502aefb36819c96a31d87696c9fc
dylan-gregory <jdylangregory@gmail.com>
2017-01-26 16:40:29 -0500

    Linking up everything

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 (function(){
   "use strict";
 
+// I want the game to automagically choose a random number upon initial page load, so this is an iife
+(function(){
+  commonWords[Math.floor(Math.random() * commonWords.length)];
 
-
+}());
 
 
 
