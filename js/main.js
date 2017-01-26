f990da0a0d9ba0464807cce9c1b80568cb863d7e
dylan-gregory <jdylangregory@gmail.com>
2017-01-26 16:50:59 -0500

    Starting the logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 (function(){
   "use strict";
 
-// I want the game to automagically choose a random number upon initial page load, so this is an iife
+// I want the game to automagically choose a random word upon initial page load, so this is an iife
+var randoWord;
+
 (function(){
-  commonWords[Math.floor(Math.random() * commonWords.length)];
+  randoWord = commonWords[Math.floor(Math.random() * commonWords.length)];
 
 }());
+console.log(randoWord);
 
+//This splits the random word into an array of it's individual characters
+var randoArray = randoWord.split("");
 
+console.log(randoArray);
 
 
 
