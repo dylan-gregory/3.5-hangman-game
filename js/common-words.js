ee4498aacadeb4a9869eb2613738b1f261a7292d
dylan-gregory <jdylangregory@gmail.com>
2017-01-27 12:20:56 -0500

    Tweaking layout and functions

--- js/common-words.js
+++ js/common-words.js
@@ -13,3 +13,9 @@ var commonWords = [
   "who","oil","its","now","find","long","down","day","did","get",
   "come","made","may","part"
 ];
+
+
+var alphabet = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
--- js/main.js
+++ js/main.js
@@ -23,6 +23,49 @@ var displayDashes = randoArray.map(function(indexItem){
 // This displays the dashes and removes the commas in between them
 document.querySelector('.word-display').innerHTML = displayDashes.join(" ");
 
+// This gives you the number of turns you'll have
+var numTurns = randoArray.length + 5;
+
+// This displays displays the above
+document.querySelector('.turns-display').innerHTML = "You've got " + numTurns + " guesses left!";
+
+var letterGuessed = document.querySelector('.btnA').textContent;
+
+document.querySelector('.btn').addEventListener("click", compareLetter);
+
+function compareLetter(letterGuessed){
+  if (randoArray.includes(letterGuessed)) {
+    console.log("Good guess!");
+  }
+};
+
+
+// var decrimentTurns = function(turns){
+//   numTurns = turns -1 ;
+//   return numTurns;
+// }
+
+// var letterGuessed = document.querySelector('.btn').textContent;
+
+// var vaildGuess;
+
+// var letterAssign = function(guess){
+//   guess == letterGuessed;
+//   if (randoArray.includes(letterGuessed)) {
+//     randoArray.map(function(indexItem){
+//       return indexItem = letterGuessed;
+//     });
+//     decrimentTurns();
+//   }
+// }
+
+// for core logic
+// var letterAssign =
+//
+// validGuess
+//     letterGuessed === validGuess
+// if (validGuess)
+//   decrimentTurns
 
 
 
